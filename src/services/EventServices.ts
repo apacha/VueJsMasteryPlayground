b0d07f6abd84fb13746ecf6a2944162b2932debb
Alexander Pacha <alexander.pacha@enote.com>
2023-08-07 16:19:46 +0200

    Playing around with types

--- src/services/EventServices.ts
+++ src/services/EventServices.ts
@@ -9,6 +9,31 @@ const apiClient = axios.create({
     }
 })
 
+// | is the union operator for defining multiple types
+type buttonType = 'primary' | 'secondary' | 'success' | 'danger'
+let buttonStyle: buttonType = "primary"
+
+type ComicUniverse = 'Marvel' | 'DC'
+interface Hero {
+    // Convention of using ; at the end
+    name: string;
+    age: number;
+    active: boolean;
+    powers: string[];
+    universe: ComicUniverse
+}
+let person: Hero = {
+    name: "Peter Parker",
+    age: 20,
+    active: true,
+    powers: ["wall-crawl", "spider-sense"],
+    universe: 'Marvel'
+}
+
+let addNumbers = (a: number, b: number): number => {
+    return a + b
+}
+
 export default {
     getEvents(perPage: number, page: number) {
         return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
