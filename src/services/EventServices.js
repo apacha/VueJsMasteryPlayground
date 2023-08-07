926f3f775dbf0440cb52a9074c887484584bec4b
Alexander Pacha <alexander.pacha@enote.com>
2023-08-07 11:14:42 +0200

    End of dynamic routing lession

--- src/services/EventServices.js
+++ src/services/EventServices.js
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
     getEvents() {
         return apiClient.get('/events')
+    },
+    getEventDetails(id) {
+        return apiClient.get('/events/' + id)
     }
 }
---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import EventServices from '@/services/EventServices'
+import { ref, onMounted } from 'vue'
+
+const event = ref(null)
+
+const props = defineProps({
+  id: {
+    required: true
+  },
+})
+
+onMounted(() => {
+  // fetch event (by id) and set local event data
+  EventServices.getEventDetails(props.id)
+    .then(
+      (response) => {
+        event.value = response.data
+      }
+    )
+    .catch(
+      (error) => {
+        console.log(error)
+      }
+    )
+})
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
