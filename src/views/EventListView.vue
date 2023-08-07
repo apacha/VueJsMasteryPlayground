404d49cd43e738fe54468e345ee6f728b7b4d16e
Alexander Pacha <alexander.pacha@enote.com>
2023-08-07 13:50:04 +0200

    Adding computed property whether we have a next page. Finished styling pagination.

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,9 +2,11 @@
   <h1>Events for myself</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <RouterLink :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page > 1">Previous</RouterLink>
+    <div class="pagination">
+      <RouterLink :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page > 1" id="page-prev">&#60; Previous</RouterLink>
 
-    <RouterLink :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">Next</RouterLink>
+      <RouterLink :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next" v-if="hasNextPage" id="page-next">Next &#62;</RouterLink>
+    </div>
   </div>
 </template>
 
@@ -15,23 +17,37 @@
 import EventCard from '@/components/EventCard.vue'
 import EventServices from '@/services/EventServices'
 import { ref, onMounted, watchEffect } from 'vue'
+import { computed } from "@vue/reactivity"
 
 const props = defineProps({
   page: {
-    required: true
+    required: true,
+    type: Number
   },
 })
 
+const hasNextPage = computed({
+  get() {
+    var totalPages = Math.ceil(totalEvents.value / pageSize)
+    return props.page < totalPages
+  }
+})
+
 const events = ref(null)
+const totalEvents = ref(null)
+const pageSize = 2
 
 onMounted(() => {
-  // watch effect wraps this thing, so it is updated, when reactive objects inside change
+  // watchEffect wraps this thing, so it is updated, when reactive objects inside change
   watchEffect(() => {
+    // Set the events to null, so the user immediately sees the effect of his action
     events.value = null
+    // Trigger a promise-based request to the API
     EventServices.getEvents(2, props.page)
       .then(
         (response) => {
           events.value = response.data
+          totalEvents.value = response.headers['x-total-count']
         }
       )
       .catch(
@@ -51,5 +67,22 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
