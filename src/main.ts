7e5742d68abfe8f746e208aadd3f9e0fe80aea3f
Alexander Pacha <alexander.pacha@enote.com>
2023-08-08 10:26:17 +0200

    Adding font-awesome and simple dragabble component into the project

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-app.use(createPinia())
-app.use(router)
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faHouse, faAlignJustify } from '@fortawesome/free-solid-svg-icons'
+
+import rawDisplayer from "@/components/raw-displayer.vue";
+
+/* add icons to the library */
+library.add(faHouse, faAlignJustify)
+
+const app = createApp(App)
+    .use(createPinia())
+    .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .component("rawDisplayer", rawDisplayer)
 
 app.mount('#app')
