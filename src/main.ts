b713d8c246309f99e2442f0af522f182db8fc4eb
Alexander Pacha <alexander.pacha@enote.com>
2023-08-08 10:38:26 +0200

    Added bootstrap and popperjs to the project

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
